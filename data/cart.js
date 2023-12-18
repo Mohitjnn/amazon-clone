8a477e84a87723c63d374305446ca8d462385a27
Mohitjnn <jnmohit29@gmail.com>
2023-12-18 06:30:57 +0530

    generate html for order summary

--- data/cart.js
+++ data/cart.js
@@ -1,24 +1,31 @@
-export const cart =[];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    quantity: 2
+},{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    productName: "Intermediate Size Basketball",
+    quantity: 1
+}];
 
 export function addToCart(productId, quantity, productName) {
     let matchingItem;
-  
+
     cart.forEach((cartItem) => {
-      if
-        (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
+        if
+            (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
     })
-  
+
     if (matchingItem) {
-      matchingItem.quantity += quantity;
+        matchingItem.quantity += quantity;
     } else {
-      cart.push({
-        productId,
-        productName,
-        quantity
-      });
+        cart.push({
+            productId,
+            productName,
+            quantity
+        });
     }
-  
-  }
-  
+
+}
