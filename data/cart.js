d36e65c695085f6e919a6c2d630b311e891e618b
Mohitjnn <jnmohit29@gmail.com>
2023-12-18 07:01:46 +0530

    delete product from cart upon pressing delete

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,8 @@
-export const cart = [{
+export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     quantity: 2
-},{
+}, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     productName: "Intermediate Size Basketball",
     quantity: 1
@@ -28,4 +28,16 @@ export function addToCart(productId, quantity, productName) {
         });
     }
 
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
 }
