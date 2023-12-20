4f945dbedce652a21a455b81bbcc97da5b710b6d
Mohitjnn <jnmohit29@gmail.com>
2023-12-20 16:36:21 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOption.js";
-
+import { renderPaymentSummary } from "./paymentSummary.js";
 export function renderOrderSummary() {
     let cartSummaryHTML = '';
     cart.forEach((cartItem) => {
@@ -118,6 +118,8 @@ export function renderOrderSummary() {
             container.remove();
 
             updateCartItem();
+
+            renderPaymentSummary();
         })
     });
 
@@ -186,7 +188,7 @@ export function renderOrderSummary() {
             const { productId, deliveryOptionId } = element.dataset;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
-
+            renderPaymentSummary();
         })
     })
 }
