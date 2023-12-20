498fe45185d70ff5d2147734487898ccb56f677a
Mohitjnn <jnmohit29@gmail.com>
2023-12-20 06:08:02 +0530

    save data and html generation for delivery date options

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,13 @@
 export let cart = JSON.parse(localStorage.getItem('cart')) || [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
 }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     productName: "Intermediate Size Basketball",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
 }];
 
 export function saveToStorage() {
@@ -28,7 +30,8 @@ export function addToCart(productId, quantity, productName) {
         cart.push({
             productId,
             productName,
-            quantity
+            quantity,
+            deliveryOptionId: '1' // Default to Delivery option 1 for now until we have a way of getting this from the server
         });
     }
 
